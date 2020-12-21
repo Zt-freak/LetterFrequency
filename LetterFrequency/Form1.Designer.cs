9f4e808981d0d06c883d38d7d823c1aa93bbd3c2
Zt-freak <bnkho98@hotmail.com>
2020-12-21 23:14:55 +0100

    initial commit

---
+++ LetterFrequency/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace LetterFrequency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Character = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(525, 425);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Character,
+            this.Amount});
+            this.dataGridView1.Location = new System.Drawing.Point(544, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(243, 425);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Character
+            // 
+            this.Character.HeaderText = "Character";
+            this.Character.Name = "Character";
+            this.Character.ReadOnly = true;
+            // 
+            // Amount
+            // 
+            this.Amount.HeaderText = "Amount";
+            this.Amount.Name = "Amount";
+            this.Amount.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Character;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+    }
+}
+
---
+++ LetterFrequency/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetterFrequency
+{
+    public partial class Form1 : Form
+    {
+        public List<DataGridViewRow> characterList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            RichTextBox textBox = (RichTextBox)sender;
+
+            this.dataGridView1.Rows.Clear();
+            this.dataGridView1.Refresh();
+
+            Console.WriteLine(this.characterList);
+
+            if (this.characterList != null)
+            {
+                this.characterList.Clear();
+            }
+
+
+            foreach (char character in textBox.Text)
+            {
+                Boolean exists = false;
+                if (this.dataGridView1.Rows.Count > 1)
+                {
+                    foreach (DataGridViewRow row in this.dataGridView1.Rows)
+                    {
+                        if (row.Cells[0].Value != null) {
+                            if (row.Cells[0].Value.ToString().Contains(character.ToString()))
+                            {
+                                exists = true;
+                                row.Cells[1].Value = Convert.ToInt32(row.Cells[1].Value) + 1;
+                            }
+                        }
+                    }
+                }
+
+                if (!exists)
+                {
+                    DataGridViewRow newRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone(); ;
+                    newRow.Cells[0].Value = character;
+                    newRow.Cells[1].Value = 1;
+                    this.dataGridView1.Rows.Add(newRow);
+                }
+            }
+        }
+    }
+}
