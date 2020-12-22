4b6c28c8a089eeaadd95b4621681805cd2676720
Zt-freak <bnkho98@hotmail.com>
2020-12-22 19:31:14 +0100

    combine character counts for capitalisable characters

--- LetterFrequency/Form1.cs
+++ LetterFrequency/Form1.cs
@@ -36,14 +36,26 @@ namespace LetterFrequency
             foreach (char character in textBox.Text)
             {
                 Boolean exists = false;
+                Boolean capitalisable = false;
+                Boolean capitalExists = false;
+
+                if (Char.IsUpper(character) || Char.ToUpper(character) != character)
+                {
+                    capitalisable = true;
+                }
+
                 if (this.dataGridView1.Rows.Count > 1)
                 {
                     foreach (DataGridViewRow row in this.dataGridView1.Rows)
                     {
                         if (row.Cells[0].Value != null) {
-                            if (row.Cells[0].Value.ToString().Contains(character.ToString()))
+                            if (row.Cells[0].Value.ToString().Equals(character.ToString()))
                             {
                                 exists = true;
+                                row.Cells[1].Value = Convert.ToInt32(row.Cells[1].Value) + 1;
+                            }
+                            if (row.Cells[0].Value.ToString().Equals(Char.ToUpper(character).ToString() + " [Total]"))
+                            {
                                 row.Cells[1].Value = Convert.ToInt32(row.Cells[1].Value) + 1;
                             }
                         }
@@ -53,9 +65,29 @@ namespace LetterFrequency
                 if (!exists)
                 {
                     DataGridViewRow newRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone(); ;
-                    newRow.Cells[0].Value = character;
+                    newRow.Cells[0].Value = character.ToString();
                     newRow.Cells[1].Value = 1;
                     this.dataGridView1.Rows.Add(newRow);
+                    if (capitalisable)
+                    {
+                        foreach (DataGridViewRow row in this.dataGridView1.Rows)
+                        {
+                            if (row.Cells[0].Value != null)
+                            {
+                                if (row.Cells[0].Value.ToString().Equals(Char.ToUpper(character).ToString() + " [Total]"))
+                                {
+                                    capitalExists = true;
+                                }
+                            }
+                        }
+                        if (!capitalExists)
+                        {
+                            DataGridViewRow newCapRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone(); ;
+                            newCapRow.Cells[0].Value = Char.ToUpper(character).ToString() + " [Total]";
+                            newCapRow.Cells[1].Value = 1;
+                            this.dataGridView1.Rows.Add(newCapRow);
+                        }
+                    }
                 }
             }
         }
